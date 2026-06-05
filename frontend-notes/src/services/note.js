7a8fe6081caacbe3f903cda0021f79fe6cc68cfb
Victoria Patarroyo <victoria.patarroyo@gmail.com>
2026-06-05 13:33:29 -0500

    Ajuste URL relativa

--- frontend-notes/src/services/note.js
+++ frontend-notes/src/services/note.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseUrl = "https://render-test-otxo.onrender.com/api/notes";
+//const baseUrl = "https://render-test-otxo.onrender.com/api/notes";
+const baseUrl = "/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
